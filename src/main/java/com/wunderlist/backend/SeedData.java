085080ecfd4dd825173d65d38167d4c506ca5506
numbers0580 <koreankid18@yahoo.com>
2020-08-22 19:36:22 -0600

    Added block comment to SeedData

--- src/main/java/com/wunderlist/backend/SeedData.java
+++ src/main/java/com/wunderlist/backend/SeedData.java
@@ -10,5 +10,39 @@ public class SeedData implements CommandLineRunner {
     @Transactional
     @Override
     public void run(String[] args) throws Exception {
+        /*
+        Users Table
+        long userid
+        String username
+        String password
+        String email
+        String firstname
+        String lastmane
+
+        Todolists Table
+        long todoid
+        String title
+        long (foreign) userid
+
+        Items Table
+        long itemid
+        String name
+        String description
+        String duedate
+        String frequency (I thought about int, but changed my mind)
+        int status
+        long (foreign) todoid
+
+        Status table
+        long statusid
+        String type
+
+        Itemstatus table (jointable)
+        long (foreign) itemid
+        long (foreign) statusid
+
+        Note: All of these tables need: created_by, created_date, last_modified_by,
+        and last_modified_date (or whatever stylization/casing desired)
+        */
     }
 }
